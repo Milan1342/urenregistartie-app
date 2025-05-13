dc21951311f2b96de0e0ac94e8593606f0683c54
Milan1342 <167193161+Milan1342@users.noreply.github.com>
2025-05-13 21:41:30 +0200

    best

--- appy.py
+++ appy.py
@@ -30,7 +30,6 @@ with col2:
     uren = st.number_input("Aantal uren", min_value=0.0, value=1.0)
 
 activiteit = st.text_input("Activiteit", placeholder="Bijv. Website bouwen")
-
 if st.button("Toevoegen"):
     if activiteit:
         nieuwe_gegevens = {
@@ -41,10 +40,20 @@ if st.button("Toevoegen"):
         }
         df = pd.concat([df, pd.DataFrame([nieuwe_gegevens])], ignore_index=True)
         df.to_csv(CSV_FILE, index=False)
-        st.success("Invoer toegevoegd!")
-        st.rerun()
+
+        # Sla tijdelijk op in session_state en herlaad
+        st.session_state["invoer_toegevoegd"] = True
+        st.experimental_rerun()
     else:
         st.warning("Vul een activiteit in.")
+
+# Herlaad gegevens na invoer
+if st.session_state.get("invoer_toegevoegd", False):
+    if os.path.exists(CSV_FILE) and os.path.getsize(CSV_FILE) > 0:
+        df = pd.read_csv(CSV_FILE, parse_dates=["Datum"])
+    st.session_state["invoer_toegevoegd"] = False
+
+
 
 # Overzicht tonen
 st.subheader("Overzicht")
